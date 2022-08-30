c04be381cd19936d847e32de23117f734e12db1e
SabinTarba <92083574+SabinTarba@users.noreply.github.com>
2022-08-30 18:10:39 +0300

    Update EmployeeService.js

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const EMPLOYEE_API_FETCH_ALL_EMPLOYEES = "https://employee-management-starba.herokuapp.com//api/v1/employees/allEmployees"
-const EMPLOYEE_API_POST_EMPLOYEE = "https://employee-management-starba.herokuapp.com//api/v1/employees/createEmployee"
-const EMPLOYEE_API_GET_EMPLOYEE_BY_ID = "https://employee-management-starba.herokuapp.com//api/v1/employees/getEmployee/"
-const EMPLOYEE_API_PUT_EMPLOYEE = "https://employee-management-starba.herokuapp.com//api/v1/employees/updateEmployee/"
-const EMPLOYEE_API_DELETE_EMPLOYEE = "https://employee-management-starba.herokuapp.com//api/v1/employees/deleteEmployee/"
+const EMPLOYEE_API_FETCH_ALL_EMPLOYEES = "https://employee-management-starba.herokuapp.com/api/v1/employees/allEmployees"
+const EMPLOYEE_API_POST_EMPLOYEE = "https://employee-management-starba.herokuapp.com/api/v1/employees/createEmployee"
+const EMPLOYEE_API_GET_EMPLOYEE_BY_ID = "https://employee-management-starba.herokuapp.com/api/v1/employees/getEmployee/"
+const EMPLOYEE_API_PUT_EMPLOYEE = "https://employee-management-starba.herokuapp.com/api/v1/employees/updateEmployee/"
+const EMPLOYEE_API_DELETE_EMPLOYEE = "https://employee-management-starba.herokuapp.com/api/v1/employees/deleteEmployee/"
 
 class EmployeeService {
 
