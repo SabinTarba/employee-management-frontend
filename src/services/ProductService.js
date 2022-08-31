62dc7f9eab40b2ea3319f03e3ec9e25097710138
SabinTarba <92083574+SabinTarba@users.noreply.github.com>
2022-09-01 01:59:13 +0300

    Update ProductService.js

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 
-const API_PRODUCT_FETCH_ALL_PRODUCTS = "http://localhost:8080/api/v1/products/allProducts/";
-const API_PRODUCT_DELETE_PRODUCT = "http://localhost:8080/api/v1/products/deleteProduct/";
-const API_PRODUCT_TOGGLE_ACTIVE = "http://localhost:8080/api/v1/products/toggleActive/";
-const API_PROUDCT_CREATE_PRODUCT = "http://localhost:8080/api/v1/products/createProduct/";
+const API_PRODUCT_FETCH_ALL_PRODUCTS = "https://employee-management-starba.herokuapp.com/api/v1/products/allProducts/";
+const API_PRODUCT_DELETE_PRODUCT = "https://employee-management-starba.herokuapp.com/api/v1/products/deleteProduct/";
+const API_PRODUCT_TOGGLE_ACTIVE = "https://employee-management-starba.herokuapp.com/api/v1/products/toggleActive/";
+const API_PROUDCT_CREATE_PRODUCT = "https://employee-management-starba.herokuapp.com/api/v1/products/createProduct/";
 
 class ProductService {
 
